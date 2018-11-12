eea2ebe5c0e1107daca2df41cd2729d69edca9ae
David <greencross.am@gmail.com>
2018-11-12 13:37:03 +0400

    Work on popup input

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,29 +1,66 @@
 import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-const mapStyles = {
-  height: '100vh',
-  width: '60vw'
+const styles = {
+  map: {
+    height: '100vh',
+    width: '60vw'
+  },
+  form: {
+    position: 'absolute'
+  }
 }
 
 class MapContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showForm: false,
+      clickPoint: {}
+    }
+  }
+
   drawDiv () {
+    const {clickPoint} = this.state;
     return (
-      <div style={{position: 'absolute', zInde: 999, top: '48%', left: '48%'}}>
-        OLOLO
+      <div style={{
+        position: 'absolute',
+        left: clickPoint.x,
+        top: clickPoint.y,
+        zIndex: 999
+      }}>
+        <form>
+          <input type="text" />
+        </form>
       </div>
     )
   }
 
+  handleClick = (p, m, e) => {
+    console.log('Event!', e.oa, e.pixel);
+    if (!this.state.showForm) {
+      this.setState({
+        showForm: true,
+        clickPoint: {...e.pixel},
+      })
+    } else {
+      this.setState({
+        showForm: false,
+        clickPoint: {}
+      })
+    }
+  }
+
   render() {
+    const {showForm} = this.state;
     return (
       <Map
-        style={mapStyles}
+        style={styles.map}
         google={this.props.google}
         zoom={14}
-        onClick={(p, m, e) => console.log('Event!', e)}
+        onClick={this.handleClick}
       >
-        {this.drawDiv()}
+        {showForm ? this.drawDiv() : 0}
       </Map>
     );
   }
